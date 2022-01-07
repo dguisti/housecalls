fcf793c127d2ebb82c522ad81f1d0c2b138b28b2
Dallin Guisti <dallin.guisti@dodecabyte.com>
2022-01-07 08:20:13 -0800

    Initial commit

---
+++ src/js/nav.js
@@ -0,0 +1,23 @@
+var navbarChange = function () {
+    if (window.scrollY > 50) {
+        document.getElementById('navbar').classList.add("navbar-scrolled");
+    }
+    else {
+        document.getElementById('navbar').classList.remove("navbar-scrolled");
+    }
+}
+
+function navbarCollapse() {
+    nav.classList.remove("expanded");
+}
+
+function navbarExpand() {
+    nav.classList.add("expanded");
+}
+
+nav = document.getElementById("navbar");
+
+nav.addEventListener("show.bs.collapse", navbarExpand);
+nav.addEventListener("hide.bs.collapse", navbarCollapse);
+
+document.addEventListener("scroll", navbarChange);
